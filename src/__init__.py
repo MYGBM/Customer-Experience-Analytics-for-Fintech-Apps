914bf21f3b5ed4cc180038e1bb2c2f7c99c4b8d7
Mariam Yohannes Gustavo <yegetamariam@gmail.com>
2025-11-30 23:16:14 +0300

    again processed furtjer-tokenization , lemmatizaion, etc, and sentimnet analysis done

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .preprocessing import ReviewPreprocessor
 from .scraper import main
 from .config import APP_IDS, BANK_NAMES, SCRAPING_CONFIG, DATA_PATHS
-
+from .sentiment import SentimentAnalysis
